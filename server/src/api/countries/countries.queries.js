b241e55135e77de7c851fb0fac1e61b00db305a6
Dmancuso1 <danegmancuso@gmail.com>
2020-12-12 13:16:41 -0500

    create route for specific country

--- server/src/api/countries/countries.queries.js
+++ server/src/api/countries/countries.queries.js
@@ -1,10 +1,27 @@
 const db = require('../../db');
 const tableNames = require('../../constants/tableNames')
 
+
+const fields = ['id', 'name', 'code'];
+
+
 // TODO: actually write the queries
+
 module.exports = {
   // we can use this find function and use elsewhere
   find() {
-    return db(tableNames.country); // like SELECT * from tableNames.tablename
+    // TODO: filter by country
+    // TODO: make a join to address table
+    return db(tableNames.country).select(fields); // like SELECT * from tableNames.tablename
+  },
+
+  // TODO: get by id
+  async get(id) {
+    const [country] = await db(tableNames.country)
+      .select(fields)
+      .where({
+        id,
+      });  
+      return country
   },
 }; 
--- server/src/api/countries/countries.routes.js
+++ server/src/api/countries/countries.routes.js
@@ -3,11 +3,36 @@ const router = express.Router();
 
 const queries = require('./countries.queries')
 
+
 // TODO: actually call the queries
+
+
 router.get('/', async (req, res) => {
   const countries = await queries.find()
   res.json(countries)
-})
+});
+
+
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    if (isNaN(id)) {
+      const error = new Error('invalid ID');
+      res.status('422'); // unprocessable entitiy
+      throw error;
+    } else {
+      const country = await queries.get(req.params.id)
+      if (country) {
+        return res.json(country)
+      };
+      return next(); // throws an error 404
+    }
+  } catch (error) {
+    next(error); // next() lets us pass down the error .
+  }
+});
+
+
 
 
 module.exports = router;
